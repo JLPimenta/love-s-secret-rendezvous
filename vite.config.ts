a148d48de02f4853ba54692512ad1fe18424f7c7
João Luiz Pimenta <85958572+JLPimenta@users.noreply.github.com>
2026-02-06 23:48:31 -0300

    Change base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/love-s-secret-rendezvous/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
